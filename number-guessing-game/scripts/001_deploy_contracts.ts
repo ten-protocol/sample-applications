870d3b2fb7713663f7a807a6aef96a69924d15e4
Moray Grieve <moray.grieve@me.com>
2023-03-02 16:49:06 +0000

    Add in guess

--- number-guessing-game/scripts/001_deploy_contracts.ts
+++ number-guessing-game/scripts/001_deploy_contracts.ts
@@ -2,9 +2,15 @@
 module.exports = async ({getNamedAccounts, deployments}) => {
     const {deploy} = deployments;
     const {app_developer} = await getNamedAccounts();
-    await deploy('ERC20', {
+    const token = await deploy('ERC20', {
         from: app_developer,
         args: [],
+        log: true,
+    });
+
+    const game = await deploy('Guess', {
+        from: app_developer,
+        args: [100, token.address],
         log: true,
     });
 };
