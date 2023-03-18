65520cf964ec29cc39530405889c0a5d88b17eb8
Moray Grieve <moray.grieve@me.com>
2023-03-18 19:15:21 +0000

    Get the target guess

--- number-guessing-game/src/get_target.py
+++ number-guessing-game/src/get_target.py
@@ -1,23 +1,32 @@
+import argparse
 from web3 import Web3
 
-appDevPK='f52e5418e349dccdda29b6ac8b0abe6576bb7713886aa85abea6181ba731f9bb'
+# the end usr and contract address for getting the slot
+endUsr='8ead642ca80dadb0f346a66cd6aa13e08a8ac7b5c6f7578d4bac96f5db01ac99'
 contractAddress='0xC0370e0b5C1A41D447BDdA655079A1B977C71aA9'
 
-web3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545/'))
-account = web3.eth.account.privateKeyToAccount(appDevPK)
-print('Owner address: %s' % account.address)
+def get_target(web3, ):
+    # get the storage of the first slot
+    slot0=web3.eth.getStorageAt(contractAddress, 0)
+    print('Slot bytes:    %s' % slot0.hex())
 
-# get the storage of the first slot
-slot0=web3.eth.getStorageAt(contractAddress, 0)
-print('Slot bytes:    %s' % slot0.hex())
+    # slot 0 last 20 bytes owner address [12:32]
+    address=slot0[12:32]
+    print('Owner:         %s' % address.hex())
 
-# slot 0 last 20 bytes owner address [12:32]
-address=slot0[12:32]
-print('Owner:         %s' % address.hex())
-
-# slot 0 11th byte is the target
-target=slot0[11]
-print('Target:        %d' % target)
+    # slot 0 11th byte is the target
+    target=slot0[11]
+    print('Target:        %d' % target)
 
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Try to cheat and get the target from storage')
+    parser.add_argument('--network', help='Either hardhat or obscuro')
+    args = parser.parse_args()
 
+    if args.network == 'hardhat':
+        web3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545/'))
+        get_target(web3)
+    elif args.network == 'obscuro':
+        web3 = Web3(Web3.HTTPProvider('http://127.0.0.1:3000/'))
+        get_target(web3)
