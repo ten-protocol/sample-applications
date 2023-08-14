78db106f54cc3bc79ab87641451924b7e221cf6a
Moray Grieve <moray.grieve@me.com>
2023-08-14 11:17:37 +0100

    Port change

--- number-guessing-game/demo-utils/wallet/wallet_run_main.sh
+++ number-guessing-game/demo-utils/wallet/wallet_run_main.sh
@@ -1,5 +1,5 @@
 rm ./we_main.bin
-./wallet_extension -nodeHost testnet.obscu.ro -nodePortHTTP 13000 -nodePortWS 13001 --port 3000 --portWS 3001 --persistencePath ./we_main.bin --logPath ./we_main.logs  -verbose
+./wallet_extension -nodeHost testnet.obscu.ro -nodePortHTTP 80 -nodePortWS 81 --port 3000 --portWS 3001 --persistencePath ./we_main.bin --logPath ./we_main.logs  -verbose
 
 
 
