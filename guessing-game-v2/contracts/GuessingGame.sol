af71b04c12721d183fcd534bdc348c892ab587c6
Jobin Babu Ayathil <jobinb6444@outlook.com>
2024-01-05 17:37:47 +0530

    Feedback Fix

--- guessing-game-v2/contracts/GuessingGame.sol
+++ guessing-game-v2/contracts/GuessingGame.sol
@@ -22,45 +22,49 @@ contract GuessingGame {
 
     constructor() {
         owner = msg.sender;
-        _resetSecretNumber();
+        _setSecretNumber();
     }
 
-    function guess(uint256 _number) external payable {
-        require(_number > 0 && _number <= MAX_GUESS, 'Secret number should be between 1 and 1000');
-        require(msg.value == GUESS_FEE, 'You need to pay a fees of 0.443 ETH to make a guess');
-        totalGuesses += 1;
+function guess(uint256 _number) external payable {
+    require(_number > 0 && _number <= MAX_GUESS, 'Secret number should be between 1 and 1000');
+    require(msg.value == GUESS_FEE, 'You need to pay a fees of 0.443 ETH to make a guess');
+    totalGuesses += 1;
 
-        if (lastGuess[msg.sender] == 0 || lastResetTime > lastGuess[msg.sender]) {
-            lastGuess[msg.sender] = 0;
-            lastDifference[msg.sender] = 0;
-        }
+    if (lastGuess[msg.sender] == 0 || lastResetTime > lastGuess[msg.sender]) {
+        lastDifference[msg.sender] = MAX_GUESS + 1;
+    }
 
-        uint256 currentDifference = _number > secretNumber ? _number - secretNumber : secretNumber - _number;
-        string memory feedback;
+    uint256 currentDifference = _number > secretNumber ? _number - secretNumber : secretNumber - _number;
+    string memory feedback;
 
-        if (_number == secretNumber) {
-            payable(msg.sender).transfer(address(this).balance);
-            feedback = "correct! You won the prize";
-            emit Guessed(msg.sender, _number, true, feedback);
-            _resetSecretNumber();
-            totalGuesses = 0;
-        } else {
-            if (lastGuess[msg.sender] != 0) {
-                if (currentDifference < lastDifference[msg.sender]) {
-                    feedback = "warmer";
-                } else if (currentDifference > lastDifference[msg.sender]) {
-                    feedback = "colder";
-                } else {
-                    feedback = "neither warmer nor colder";
-                }
+    if (_number == secretNumber) {
+        payable(msg.sender).transfer(address(this).balance);
+        feedback = "correct! You won the prize";
+        emit Guessed(msg.sender, _number, true, feedback);
+        _resetSecretNumber();
+        totalGuesses = 0;
+    } else {
+        if (lastGuess[msg.sender] != 0 && lastResetTime <= lastGuess[msg.sender]) {
+            if (currentDifference < lastDifference[msg.sender]) {
+                feedback = "warmer";
+            } else if (currentDifference > lastDifference[msg.sender]) {
+                feedback = "colder";
             } else {
-                feedback = "first guess, try again!";
+                feedback = "neither warmer nor colder";
             }
-
-            lastGuess[msg.sender] = _number;
-            lastDifference[msg.sender] = currentDifference;
-            emit Guessed(msg.sender, _number, false, feedback);
+        } else {
+            feedback = "first guess, try again!";
         }
+
+        lastGuess[msg.sender] = block.timestamp;
+        lastDifference[msg.sender] = currentDifference;
+        emit Guessed(msg.sender, _number, false, feedback);
+    }
+}
+
+    function _setSecretNumber() private {
+        uint256 randomNumber = block.difficulty;
+        secretNumber = (randomNumber % MAX_GUESS) + 1;
     }
 
     function _resetSecretNumber() private {
