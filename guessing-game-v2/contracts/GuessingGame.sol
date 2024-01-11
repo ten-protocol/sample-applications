d62a46f1bcd155b4a37bf1a4a5b679eb16424dfd
Jobin Babu Ayathil <jobinb6444@outlook.com>
2024-01-11 14:31:27 +0530

    Add Comments

--- guessing-game-v2/contracts/GuessingGame.sol
+++ guessing-game-v2/contracts/GuessingGame.sol
@@ -5,15 +5,14 @@ contract GuessingGame {
     uint256 private secretNumber;
     address private owner;
     uint256 public totalGuesses;
-    mapping(address => uint256) private lastGuess;
-    mapping(address => uint256) private lastDifference;
+    mapping(address => uint256) private lastGuess; // Stores the timestamp of the last guess made by each player.
+    mapping(address => uint256) private lastDifference; // Stores the difference between the last guess and the secret number for each player.
 
-    // constants
-    uint256 public constant GUESS_FEE = 443e14; // 0.0443 ETH
+    uint256 public constant GUESS_FEE = 443e14;
     uint256 public constant MAX_GUESS = 1000;
-    uint256 private lastResetTime;
+    uint256 private lastResetTime; // Stores the timestamp of the last game reset.
 
-    event Guessed(address indexed user, uint256 guessedNumber, bool success, string feedback);
+    event Guessed(address indexed user, uint256 guessedNumber, bool success, string feedback); // Event triggered when a player makes a guess.
 
     modifier onlyOwner() {
         require(msg.sender == owner, 'Only the owner can call this function');
@@ -22,50 +21,50 @@ contract GuessingGame {
 
     constructor() {
         owner = msg.sender;
-        _setSecretNumber();
+        _setSecretNumber(); // Initialize the secret number when the contract is deployed.
     }
 
-function guess(uint256 _number) external payable {
-    require(_number > 0 && _number <= MAX_GUESS, 'Secret number should be between 1 and 1000');
-    require(msg.value == GUESS_FEE, 'You need to pay a fees of 0.443 ETH to make a guess');
-    totalGuesses += 1;
+    function guess(uint256 _number) external payable {
+        require(_number > 0 && _number <= MAX_GUESS, 'Secret number should be between 1 and 1000');
+        require(msg.value == GUESS_FEE, 'You need to pay a fee of 0.443 ETH to make a guess');
+        totalGuesses += 1;
 
-    if (lastResetTime > lastGuess[msg.sender]) {
-        lastDifference[msg.sender] = MAX_GUESS + 1;
-    }
-
-    uint256 currentDifference = _number > secretNumber ? _number - secretNumber : secretNumber - _number;
-    string memory feedback;
-
-    if (_number == secretNumber) {
-        payable(msg.sender).transfer(address(this).balance);
-        feedback = "correct! You won the prize";
-        emit Guessed(msg.sender, _number, true, feedback);
-        _setSecretNumber();
-        lastResetTime = block.timestamp;
-        totalGuesses = 0;
-    } else {
         if (lastResetTime > lastGuess[msg.sender]) {
-            if (currentDifference < lastDifference[msg.sender]) {
-                feedback = "warmer";
-            } else if (currentDifference > lastDifference[msg.sender]) {
-                feedback = "colder";
-            } else {
-                feedback = "neither warmer nor colder";
-            }
-        } else {
-            feedback = "first guess, try again!";
+            lastDifference[msg.sender] = MAX_GUESS + 1; // Reset the last difference if the player is guessing after a secret no. reset.
         }
 
-        lastGuess[msg.sender] = block.timestamp;
-        lastDifference[msg.sender] = currentDifference;
-        emit Guessed(msg.sender, _number, false, feedback);
+        uint256 currentDifference = _number > secretNumber ? _number - secretNumber : secretNumber - _number;
+        string memory feedback;
+
+        if (_number == secretNumber) {
+            payable(msg.sender).transfer(address(this).balance);
+            feedback = "correct! You won the prize";
+            emit Guessed(msg.sender, _number, true, feedback);
+            _setSecretNumber();
+            lastResetTime = block.timestamp; // Record the timestamp of the last game reset.
+            totalGuesses = 0; // Reset the total number of guesses made.
+        } else {
+            if (lastResetTime > lastGuess[msg.sender]) {
+                if (currentDifference < lastDifference[msg.sender]) {
+                    feedback = "warmer";
+                } else if (currentDifference > lastDifference[msg.sender]) {
+                    feedback = "colder";
+                } else {
+                    feedback = "neither warmer nor colder";
+                }
+            } else {
+                feedback = "first guess, try again!";
+            }
+
+            lastGuess[msg.sender] = block.timestamp; // Record the timestamp of the last guess made by the player.
+            lastDifference[msg.sender] = currentDifference; // Update the last difference for the player.
+            emit Guessed(msg.sender, _number, false, feedback);
+        }
     }
-}
 
     function _setSecretNumber() private {
         uint256 randomNumber = block.difficulty;
-        secretNumber = (randomNumber % MAX_GUESS) + 1;
+        secretNumber = (randomNumber % MAX_GUESS) + 1; // Generate a new random secret number.
     }
 
     function getContractBalance() external view returns (uint256) {
