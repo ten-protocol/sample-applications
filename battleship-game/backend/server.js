49597dbbb33b05e81df119f8f32b827c60377358
Jennifer Echenim <echenimjennifer@gmail.com>
2024-01-11 15:42:43 +0400

    Update server port

--- battleship-game/backend/server.js
+++ battleship-game/backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 dotenv.config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 const battleshipRoutes = require("./routes/battleshipRoutes");
 
 const connectDB = require("./config/db");
@@ -17,5 +17,5 @@ app.use("/", battleshipRoutes);
 connectDB();
 
 app.listen(port, () => {
-	console.log(`Server running on port: ${port}`);
+  console.log(`Server running on port: ${port}`);
 });
