1dba0e4d2d7a675c4e788e565e18687c90742250
Jennifer Echenim <echenimjennifer@gmail.com>
2024-01-25 12:53:24 +0400

    wip: web3 service setup

--- spot-the-ball/src/store/gameStore.ts
+++ spot-the-ball/src/store/gameStore.ts
@@ -1,9 +1,24 @@
 import { defineStore } from 'pinia'
+/* import the ipfs-http-client library */
+import { create as ipfsHttpClient } from 'ipfs-http-client'
+/* Create an instance of the client */
+const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
+/* upload the file */
+// const added = await client.add(file)
+// /* or a string */
+// const added = await client.add('hello world')
 
 export const useBattleStore = defineStore('battleStore', {
   state: () => ({}),
 
   getters: {},
 
-  actions: {}
+  actions: {
+    async uploadFile(file: any) {
+      const added = await client.add(file)
+      console.log(added)
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`
+      console.log(url)
+    }
+  }
 })
