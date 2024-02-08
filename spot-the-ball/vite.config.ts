f807d4fa53c49089b0567f5f52a0c442d5bcca7f
Jennifer Echenim <echenimjennifer@gmail.com>
2024-02-08 17:45:15 +0400

    - update env declaration and vite config - add dist folder to .gitignore

--- spot-the-ball/vite.config.ts
+++ spot-the-ball/vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
@@ -11,9 +11,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  define: {
-    global: {}
   }
   // base: '/sample-applications/spot-the-ball/'
 })
