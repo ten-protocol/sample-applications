dc3c1d5700f514fc026fe3ed2c0f8ee63cd87b92
Jennifer Echenim <echenimjennifer@gmail.com>
2024-02-19 18:13:46 +0400

    Fix routing issues and add admin link

--- spot-the-ball/src/views/Home.vue
+++ spot-the-ball/src/views/Home.vue
@@ -6,6 +6,10 @@
     <Controls />
     </div> -->
     <History />
+    <!-- adding admin link to navigate to admin page b'cos using gh-pages w/ SPAs doesn't work well
+    <router-link to="/admin" class="fixed bottom-4 right-4">
+      <div class="w-2 h-2 bg-gray-100 rounded-full flex items-center justify-center"></div>
+    </router-link> -->
   </main>
 </template>
 
@@ -13,4 +17,12 @@
 import Game from '../components/Game.vue'
 import Header from '../components/Header.vue'
 import History from '../components/History.vue'
+
+mounted(() => {
+  let path = localStorage.getItem('path')
+  if (path) {
+    localStorage.removeItem('path')
+    this.router.navigate([path])
+  }
+})
 </script>
