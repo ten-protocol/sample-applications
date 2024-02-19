fa3ce754c6cdcf50198e41ecbc264d6d8540d235
Jennifer Echenim <echenimjennifer@gmail.com>
2024-02-19 15:26:11 +0100

    Update circle size (#70)
    * Update circle size
    
    * Fix routing issues and add admin link

--- spot-the-ball/src/views/Home.vue
+++ spot-the-ball/src/views/Home.vue
@@ -6,11 +6,25 @@
     <Controls />
     </div> -->
     <History />
+    <!-- adding admin link to navigate to admin page b'cos using gh-pages w/ SPAs doesn't work well -->
+    <router-link to="/admin" class="fixed bottom-4 right-4">
+      <div class="w-2 h-2 bg-gray-100 rounded-full flex items-center justify-center"></div>
+    </router-link>
   </main>
 </template>
 
-<script setup>
+<script >
 import Game from '../components/Game.vue'
 import Header from '../components/Header.vue'
 import History from '../components/History.vue'
+
+export default {
+  name: 'Home',
+
+  components: {
+    Game,
+    Header,
+    History
+  }
+}
 </script>
