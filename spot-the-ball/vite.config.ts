a4a8b15f18584a8bd9767cec0e9b7cb9e37ec006
Jennifer Echenim <echenimjennifer@gmail.com>
2024-02-20 13:53:33 +0100

    update moralis key (#71)
    * Add MORALIS_API_KEY to constants

--- spot-the-ball/vite.config.ts
+++ spot-the-ball/vite.config.ts
@@ -1,17 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-import dotenv from 'dotenv'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
-dotenv.config()
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
-  define: {
-    __MORALIS_API_KEY__: process.env.VITE_APP_MORALIS_API_KEY
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
