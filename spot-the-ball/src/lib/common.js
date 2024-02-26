58938101cde9287231ace807082edad4f25725a5
Jennifer Echenim <echenimjennifer@gmail.com>
2024-02-26 07:14:32 +0100

    Update common.js

--- spot-the-ball/src/lib/common.js
+++ spot-the-ball/src/lib/common.js
@@ -1,4 +1,4 @@
 export default class Common {
   static ENTRY_COST = '0.0443'
-  static CONTRACT_ADDRESS = '0x13b1A4Cd60dc857B87930e309D4DBa2a1F0F4A51'
+  static CONTRACT_ADDRESS = 'Oxe03D05a56d35D7c87Ea0578A27C5d4fdF1C81c63'
 }
