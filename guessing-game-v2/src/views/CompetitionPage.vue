15d97b4c53955a6bca7cec71a3901f19a0ae1007
Jennifer Echenim <echenimjennifer@gmail.com>
2024-03-25 21:31:57 +0400

    Add back button to CompetitionPage

--- guessing-game-v2/src/views/CompetitionPage.vue
+++ guessing-game-v2/src/views/CompetitionPage.vue
@@ -1,5 +1,12 @@
 <template>
   <main class="center-content">
+    <router-link to="/" class="">
+      <el-button class="btn btn-primary">
+        <el-icon><Back /></el-icon>
+        <span> </span>
+        Back To Guessing Game
+      </el-button>
+    </router-link>
     <meta-mask-connect-button class="metamask-button" :scope="'competition'"
       >Connect To Metamask</meta-mask-connect-button
     >
