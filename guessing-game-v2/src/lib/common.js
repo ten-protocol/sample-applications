c6cd44574fa46105aa90fb7980e7eb64901df04a
Jennifer Echenim <echenimjennifer@gmail.com>
2024-03-26 20:53:24 +0400

    Delete unused files and configurations

--- guessing-game-v2/src/lib/common.js
+++
@@ -1,4 +0,0 @@
-export default class Common {
-  static GUESS_COST = '0.0443'
-  static COMPETITION_GUESS_COST = '0.02'
-}
--- guessing-game-v2/vite.config.js
+++ guessing-game-v2/vite.config.js
@@ -1,17 +1,3 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  base: '/sample-applications/guessing-game-v2/',
-})
+export default {
+  base: '/sample-applications/guessing-game-v2/'
+}
