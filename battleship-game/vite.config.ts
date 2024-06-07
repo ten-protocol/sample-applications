b49d7b32fbb1d8753d70ddbb5aa8584514e4ce76
Jennifer Echenim <echenimjennifer@gmail.com>
2024-06-07 01:33:28 +0100

    [BS] add Vercel config and update Vite config

--- battleship-game/vite.config.ts
+++ battleship-game/vite.config.ts
@@ -4,13 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  base: '/sample-applications/battleship-game/'
+  }
 })
