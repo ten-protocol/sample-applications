381aee92eb0e2a25bb1119f74360df9acdff9ddb
Jennifer Echenim <echenimjennifer@gmail.com>
2024-06-07 01:55:35 +0100

    Remove base path in router configuration

--- battleship-game/src/router/index.ts
+++ battleship-game/src/router/index.ts
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
-const base = '/sample-applications/battleship-game/'
+// const base = '/sample-applications/battleship-game/'
 
 const router = createRouter({
-  history: createWebHistory(base),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
