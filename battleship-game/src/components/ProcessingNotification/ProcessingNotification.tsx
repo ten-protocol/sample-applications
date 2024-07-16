cbe288fd919ebae05fe51862e56edd42aed79edc
PeterB <p.bienek@gmail.com>
2024-07-16 09:31:18 +0100

    WIP: prevent page from shifting down when notification window opens

--- battleship-game/src/components/ProcessingNotification/ProcessingNotification.tsx
+++ battleship-game/src/components/ProcessingNotification/ProcessingNotification.tsx
@@ -84,7 +84,7 @@ export default function ProcessingNotification() {
     };
 
     return (
-        <div className="-mt-60">
+        <div className="-mt-60 mb-60">
         <HudWindow
             headerTitle="Striking coordinates"
             speed={0.1}
