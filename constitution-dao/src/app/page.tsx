62b2dbd15632e26f4506cc0a5f27332dbe2c865b
Jennifer Echenim <echenimjennifer@gmail.com>
2024-10-22 03:12:38 +0400

    Add dependencies and update dev script

--- constitution-dao/src/app/page.tsx
+++ constitution-dao/src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ThresholdIntentAuction from "./components/common/ThresholdIntentAuction";
 import { WagmiProvider } from "wagmi";
@@ -6,13 +8,14 @@ import { wagmiConfig } from "../lib/wagmiConfig";
 
 export default function Home() {
   const queryClient = new QueryClient();
+
   return (
-    <WagmiProvider config={wagmiConfig}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>
-          <ThresholdIntentAuction />
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    // <WagmiProvider config={wagmiConfig}>
+    <QueryClientProvider client={queryClient}>
+      {/* <RainbowKitProvider> */}
+      <ThresholdIntentAuction />
+      {/* </RainbowKitProvider> */}
+    </QueryClientProvider>
+    // </WagmiProvider>
   );
 }
