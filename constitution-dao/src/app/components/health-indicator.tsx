df8d4dc4b22b9eb4c569a376b15302c10eef0301
Jennifer Echenim <echenimjennifer@gmail.com>
2024-10-22 06:45:30 +0400

    Add new components and providers

---
+++ constitution-dao/src/app/components/health-indicator.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import React from "react";
+import { Badge, badgeVariants } from "./ui/badge";
+import { Skeleton } from "./ui/skeleton";
+import { fetchTestnetStatus } from "@/api/general";
+import { useQuery } from "@tanstack/react-query";
+
+const HealthIndicator = () => {
+  const [status, setStatus] = React.useState<boolean>(false);
+
+  const { data: testnetStatus, isLoading: isStatusLoading } = useQuery({
+    queryKey: ["testnetStatus"],
+    queryFn: () => fetchTestnetStatus(),
+    refetchInterval: 10000,
+  });
+  const { result, errors } = testnetStatus || {};
+
+  React.useEffect(() => {
+    if (result) {
+      setStatus(true);
+      //@ts-ignore
+    } else if (errors?.includes("[p2p]")) {
+      setStatus(true);
+    } else {
+      setStatus(false);
+    }
+  }, [result, errors]);
+
+  return (
+    <div className="flex items-center space-x-2 border rounded-lg p-2">
+      <h3 className="text-sm">Testnet Status: </h3>
+      <Badge
+        variant={
+          (isStatusLoading
+            ? "secondary"
+            : status
+            ? "success"
+            : "destructive") as keyof typeof badgeVariants
+        }
+      >
+        {isStatusLoading ? (
+          <Skeleton className="w-5 h-5" />
+        ) : status ? (
+          "Live"
+        ) : (
+          "Down"
+        )}
+      </Badge>
+    </div>
+  );
+};
+
+export default HealthIndicator;
