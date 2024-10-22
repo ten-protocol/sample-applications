7af18c421b41fc36d73741112998cc7a1dd57da2
Jennifer Echenim <echenimjennifer@gmail.com>
2024-10-22 06:49:03 +0400

    Add document component and update layout and page components

--- constitution-dao/src/app/page.tsx
+++ constitution-dao/src/app/page.tsx
@@ -1,21 +1,7 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ThresholdIntentAuction from "./components/common/ThresholdIntentAuction";
-// import { WagmiProvider } from "wagmi";
-// import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
-// import { wagmiConfig } from "../lib/wagmiConfig";
 
 export default function Home() {
-  const queryClient = new QueryClient();
-
-  return (
-    // <WagmiProvider config={wagmiConfig}>
-    <QueryClientProvider client={queryClient}>
-      {/* <RainbowKitProvider> */}
-      <ThresholdIntentAuction />
-      {/* </RainbowKitProvider> */}
-    </QueryClientProvider>
-    // </WagmiProvider>
-  );
+  return <ThresholdIntentAuction />;
 }
